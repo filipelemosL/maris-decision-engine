415a5a2ef69cedecca3a97aa4a2152862926ca09
Filipe Lemos <filipelemos1415@gmail.com>
2026-03-04 19:50:10 -0300

    feat: ambiente e bibliotecas

---
+++ MDE/domain/entities/weather.py
@@ -0,0 +1,11 @@
+class Weather:
+
+    def __init__(self, timestamp, wind_speed, wind_direction,
+                 wave_height, pressure, sea_temp, current_speed):
+        self.timestamp = timestamp
+        self.wind_speed = wind_speed
+        self.wind_direction = wind_direction
+        self.wave_height = wave_height
+        self.pressure = pressure
+        self.sea_temp = sea_temp
+        self.current_speed = current_speed
